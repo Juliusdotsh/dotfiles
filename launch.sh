df3f434c924d13838041d2b6e08771b20af55466
Juliusdotsh <julius@archOne.localdomain>
2020-03-31 17:45:14 +0200

    modified dir structure + modified launch.sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
+#THIS WILL DELETE FOLDERS IN .config be careful
+
 rm LICENSE README.md
 
-yay -S rofi polybar picom git 
+# essentials
+yay -S git playerctl redshift ttf-font-awesome
 
-mv --force i3 ../
-mv --force kitty ../
-mv --force polybar ../
-mv --force rofi ../
+#gtk ricing stuff
+yay -S lxappearance arc-icon-theme nordic-theme-git
+
+#i3 ricing stuff
+yay -S rofi polybar picom 
+
+#spotify ricing stuff
+yay -S spotify spicetify-cli spicetify-themes-git
+
+
+
+
+rm -rf ~/.config/i3 
+rm -rf ~/.config/kitty 
+rm -rf ~/.config/polybar 
+rm -rf ~/.config/rofi 
+
+mv --force .config/i3 ~/.config/i3 
+mv --force .config/kitty ~/.config/kitty 
+mv --force .config/polybar ~/.config/polybar 
+mv --force .config/rofi ~/.config/rofi 
+
+#configuring spicetify
+sudo chmod 777 /opt/spotify -R
+
+spicetify backup apply
+
+#ending installation
+cd ..
+rm -rf dotfiles/
